8f5dd15d3355d0f24a44d4b9e0e1e03ce6759ad1
Daniel Lindemann <daniel@dlindemann.de>
2025-10-16 16:29:02 +0000

    clean onCreateCommand.sh

--- src/dotnet-usersecrets-persistence/onCreateCommand.sh
+++ src/dotnet-usersecrets-persistence/onCreateCommand.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 set -e
 
-# FEATURE_SCRIPTS_DIR="/usr/local/share/daniellindemann-devcontainer-features/dotnet-usersecrets-persistence/scripts"
-
-# if command -v sudo > /dev/null; then
-#     sudo chown -R "$(id -u):$(id -g)" "$FEATURE_SCRIPTS_DIR"
-# else
-#     chown -R "$(id -u):$(id -g)" "$FEATURE_SCRIPTS_DIR"
-# fi
-
-echo "In OnCreate script"
-
 echo "Activating feature 'dotnet-usersecrets-persistence'"
-echo "User: $(id -un)"
-echo "User home: $HOME"
+# echo "User: $(id -un)"
+# echo "User home: $HOME"
 
+
+echo "Fixing user permissions"
 # own the mounted volume
 if command -v sudo > /dev/null; then
     sudo chown -R "$(id -u):$(id -g)" /dc/dotnet-usersecrets
@@ -22,8 +14,11 @@ else
     chown -R "$(id -u):$(id -g)" /dc/dotnet-usersecrets
 fi
 
+echo "Setting up user secrets persistence"
 # ensure the .microsoft directory exists
 mkdir -p "${HOME}/.microsoft"
 
 # create symbolic link for user secrets storage
 ln -s /dc/dotnet-usersecrets "${HOME}/.microsoft/usersecrets"
+
+echo "Feature 'dotnet-usersecrets-persistence' activated"
